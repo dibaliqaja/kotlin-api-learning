4d1a7c687a6b8979403079eb2102c1a758b8aa34
dibaliqaja <dibaliqaja@gmail.com>
2021-03-24 17:12:34 +0700

    add data class main model

---
+++ app/src/main/java/com/example/kotlinapi/MainModel.kt
@@ -0,0 +1,3 @@
+package com.example.kotlinapi
+
+data class MainModel (val id: Int, val title: String, val url: String)
--- app/src/main/java/com/example/kotlinapi/retrofit/ApiEndpoint.kt
+++ app/src/main/java/com/example/kotlinapi/retrofit/ApiEndpoint.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinapi.retrofit
 
+import com.example.kotlinapi.MainModel
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiEndpoint {
     @GET("photos")
-    fun getPhotos()
+    fun getPhotos(): Call<List<MainModel>>
 }
